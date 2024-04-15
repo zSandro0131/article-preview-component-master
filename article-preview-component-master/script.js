59b4330ebb75712ecf736fa73ef40aaff017475a
zSandro0131 <sandryphelipe2015@gmail.com>
2024-04-15 16:12:00 -0300

    updated version of project

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -1,15 +1,15 @@
-const shareIcon = document.querySelector('.shareIcon')
-const modal = document.querySelector('.modal')
-const triangle = document.querySelector('.modal-triangle')
+const shareIcon = document.querySelector(".shareIcon");
+const modal = document.querySelector(".modal");
+const triangle = document.querySelector(".modal-triangle");
 
 function Modal() {
-   if(modal.classList.contains('active')) {
-    modal.classList.remove('active')
-    triangle.classList.remove('active')
-   } else {
-    modal.classList.add('active')
-    triangle.classList.add('active')
-   }
+  if (modal.classList.contains("active")) {
+    modal.classList.remove("active");
+    triangle.classList.remove("active");
+  } else {
+    modal.classList.add("active");
+    triangle.classList.add("active");
+  }
 }
 
-shareIcon.addEventListener('click' , Modal)
+shareIcon.addEventListener("click", Modal);
